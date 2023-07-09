ffaa73da1479ef60b3fb564ebbe972ad2347a536
Snehasish Das <78856467+snehasishdasz@users.noreply.github.com>
2023-07-09 23:49:13 +0530

    useEffect

--- Practice-Project/useEffectP/useeffectpractise/src/App.js
+++ Practice-Project/useEffectP/useeffectpractise/src/App.js
@@ -1,11 +1,22 @@
 
 import './App.css';
 import Header from "./components/Header";
+import React, {useState, useEffect} from 'react';
+
 
 function App() {
+
+  const[state,setState]= useState(0);
+
+  useEffect(()=>{
+    console.log("useEffect Called");
+  })
+  console.log("Function body");
+
   return (
     <div className="App">
       <Header />
+      <button onClick={()=>setState(state+1)}>Clickme {state}</button>
     </div>
   );
 }
