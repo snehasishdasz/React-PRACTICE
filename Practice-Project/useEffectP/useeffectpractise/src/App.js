4d8f7c3eab9286e3557db63613b771a176f4ef25
Snehasish Das <78856467+snehasishdasz@users.noreply.github.com>
2023-07-10 17:42:48 +0530

    useEffect-done

--- Practice-Project/useEffectP/useeffectpractise/src/App.js
+++ Practice-Project/useEffectP/useeffectpractise/src/App.js
@@ -6,18 +6,43 @@ import React, {useState, useEffect} from 'react';
 
 function App() {
 
-  const[state,setState]= useState(0);
+  const[state,setState]= useState(2);
+  const[data, setData] = useState();
 
-  useEffect(()=>{
-    console.log("useEffect Called");
-  })
-  console.log("Function body");
+  useEffect(() => {
+    async function getData(){
+      const get = await fetch(`https://hub.dummyapis.com/employee?noofRecords=${state}&idStarts=1001`);
+
+      const res = await get.json();
+      setData(res);
+      console.log(res);
+    }
+    getData();
+    document.title = `(${state}) Employees online`;
+  },[state])
+
+  // [] means dependency is null so no performance is work on the page.
+  // [state] means useEffect is only work for "state" property which is used in 1st button.
+
 
   return (
-    <div className="App">
+    <>
       <Header />
       <button onClick={()=>setState(state+1)}>Clickme {state}</button>
-    </div>
+      {
+        data.map((element,index)=>{
+          return(
+            <div className='data' key={index}>
+              <h4 className='data1'>{element.firstName}</h4>
+              <h4 className='data2'>{element.lastName}</h4>
+              <h4 className='data3'>{element.email}</h4>
+              
+            </div>
+          )
+        })
+      }
+
+    </>
   );
 }
 
