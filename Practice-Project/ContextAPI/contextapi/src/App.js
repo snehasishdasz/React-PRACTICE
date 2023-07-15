40ff3d00b756ed3fd7ebd3a6682b88a967aa003a
Snehasish Das <78856467+snehasishdasz@users.noreply.github.com>
2023-07-16 00:18:11 +0530

    Context-API

--- Practice-Project/ContextAPI/contextapi/src/App.js
+++ Practice-Project/ContextAPI/contextapi/src/App.js
@@ -1,23 +1,28 @@
 import Header from "./components/Header"
 import './App.css';
-import { useState, createContext } from "react";
+import { useState, createContext, useContext } from "react";
 import ComA from "./components/ComA";
 // import ComB from "./components/ComB";
 // import ComC from "./components/ComC";
 
 const AppState = createContext();
+const NameContext = createContext();   
 
 function App() {
-  const[data,setData]=useState("xyz")
+  const[Data,setData]=useState("Details");
+  const [Name,setName]=useState({name:"Snehasish",age:21});
   return (
     <>
-    <AppState.Provider value={data}>
+    <AppState.Provider value={{Data}}>
+  <NameContext.Provider value={{Name}}>
       <Header/>
       <ComA  />
+    </NameContext.Provider>
     </AppState.Provider>
+
     </>
   );
 }
 
 export default App;
-export {AppState}
+export {AppState,NameContext}
--- Practice-Project/ContextAPI/contextapi/src/components/ComC.jsx
+++ Practice-Project/ContextAPI/contextapi/src/components/ComC.jsx
@@ -1,10 +1,17 @@
-import React from 'react'
+import React ,{useContext} from 'react'
+import{AppState} from "../App"
+import{NameContext} from "../App"
+
 
 const ComC = () => {
+    const appData = useContext(AppState)
+    const nameData = useContext(NameContext)
   return (
     <div className='compc'>
         <div>ComC</div>
-        <h1></h1>
+        <h1>{appData.Data}</h1>
+        <h1>Name:{nameData.Name.name}</h1>
+        <h1>Age:{nameData.Name.age}</h1>
     </div>
   )
 }
