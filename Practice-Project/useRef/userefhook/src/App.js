f5b60c8d02ad8fdf4706b397edac0d5c548dd3de
Snehasish Das <78856467+snehasishdasz@users.noreply.github.com>
2023-07-18 02:54:10 +0530

    useRef

--- Practice-Project/useRef/userefhook/src/App.js
+++ Practice-Project/useRef/userefhook/src/App.js
@@ -5,26 +5,38 @@ import { useEffect, useRef, useState } from 'react';
 function App() {
   const [input,setInput]= useState("");
   // const[counter,setCounter]= useState(0);
-  const counter = useRef(0);
+  // const counter = useRef(0);
+  // const prevState = useRef("");
+  const inputField = useRef();
 
-  useEffect(()=>{
-    // console.log("rendered");
-    // setCounter(counter+1);
-    
-  })
+  // useEffect(()=>{
+  //   // console.log("rendered");
+  //   // setCounter(counter+1);
+  //   // counter.current = counter.current+1;
+  //   prevState.current = input;
+  // },[input])
 
   const formHandler = (e) => {
     setInput(e.target.value);    
     // "e.target.value" means we can get the value which we will write on the  input box
   }
 
-
+const clickHandler= () =>{
+  // inputField.current.focus();
+  // inputField.current.value ='BIKASH';
+  // console.log(inputField.current.value)
+    inputField.current.style.border="2px solid red"
+}
 
   return (
     <>
       <Header/>
-      <input value={input} onChange={formHandler} />
-      <h4>Application has been rendered {counter} times</h4>
+      <input ref={inputField} value={input} onChange={formHandler} />
+      {/* <h4>Application has been rendered {counter.current} times</h4>
+       */}
+      {/* <h4>Prev State was {prevState.current} </h4> */}
+      <button onClick={clickHandler}>Click Me</button>
+
     </>
   );
 }
